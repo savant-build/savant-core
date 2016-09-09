d8771cc23eea1529e231a1ffbadf924330daed9e
Brian Pontarelli <brian@inversoft.com>
2016-09-09 10:26:34 -0600

    Added JAVA_HOME handling to the scripts

--- src/main/scripts/sb.bat
+++ src/main/scripts/sb.bat
@@ -2,6 +2,7 @@
 
 IF "%OS%"=="Windows_NT" @SETLOCAL
 IF "%OS%"=="WINNT" @SETLOCAL
+IF "%JAVA_HOME%"=="" GOTO javaHomeMissing
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
@@ -11,9 +12,13 @@ IF NOT ERRORLEVEL 1 GOTO savantHomeSet
 ECHO Unable to locate SAVANT_HOME. Please set the environmental variable.
 EXIT /B 1
 
+:javaHomeMissing
+ECHO The JAVA_HOME environment variable is not set. Please set this to point to a JDK 8 installation of Java such that $JAVA_HOME/bin/java points to the Java executable
+EXIT /B 1
+
 :savantHomeSet
 SET CLASSPATH=
 FOR %%f IN ("%SAVANT_HOME%\lib\*.jar") DO SET CLASSPATH=%%f;!CLASSPATH!
-java %SAVANT_OPTS% -cp "%CLASSPATH%" org.savantbuild.run.Main %*
+%JAVA_HOME%\bin\java %SAVANT_OPTS% -cp "%CLASSPATH%" org.savantbuild.run.Main %*
 
 ENDLOCAL
